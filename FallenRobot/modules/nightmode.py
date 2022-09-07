370ab4e7db02fd69f92562c944857f7d0d982269
ᴀɴᴏɴʏᴍᴏᴜs <rahulking25102006@gmail.com>
2022-09-08 00:13:02 +0530

    Update nightmode.py

--- FallenRobot/modules/nightmode.py
+++ FallenRobot/modules/nightmode.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telethon import functions, types
 from telethon.tl.types import ChatBannedRights
 
-from FallenRobot import telethn as tbot
+from FallenRobot import telethn as tbot, BOT_NAME
 from FallenRobot.events import register
 from FallenRobot.modules.sql.night_mode_sql import (
     add_nightmode,
@@ -109,7 +109,7 @@ async def job_close():
         try:
             await tbot.send_message(
                 int(warner.chat_id),
-                "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Fallen Robot**",
+                f"**Night Mode Started**\n\n`Group Is Closing Till 6 Am, Only admins can messages in this chat.`\n\n__Powered By {BOT_NAME}__",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
@@ -134,7 +134,7 @@ async def job_open():
         try:
             await tbot.send_message(
                 int(warner.chat_id),
-                "06:00 Am, Group Is Opening.\n**Powered By Fallen Robot**",
+                f"**Night Mode Ended**\n\n`Group is opening again now everyone can send messages in this chat.`\n__Powered By {BOT_NAME}__",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
