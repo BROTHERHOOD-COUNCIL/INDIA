fd5ba1b0ff677e6145113043e5c54eaad301e34a
Mr.Programmer <109902449+MrProgrammer72@users.noreply.github.com>
2022-10-10 07:01:19 +0530

    Update userinfo.py

--- FallenRobot/modules/userinfo.py
+++ FallenRobot/modules/userinfo.py
@@ -324,10 +324,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/AnonAssociation/7"
+                                "ᴄʜᴀɴɴᴀʟ", url="https://t.me/black_network_official"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪꜱᴀꜱᴛᴇʀ", url="https://t.me/AnonAssociation/8"
+                                "ꜱᴜᴘᴘᴏʀᴛ", url="https://t.me/black_network_official"
                             ),
                         ],
                     ]
